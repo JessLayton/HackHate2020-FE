0c8e7961c7dc6ea4206139218ee89fc82c63ad64
lukecottenham <36683023+lukecottenham@users.noreply.github.com>
2020-10-29 17:10:10 +0000

    User settings (#5)
    * basic settings drawer added
    
    * theme added to app
    
    * theme changer added
    
    * buttons to change themes in menu
    
    * set theme colour, font size and font from settings
    
    * User settings
    
    * drawer no longer always open
    
    * changes
    
    * changes font size for test text only
    
    * Set font size button
    
    * import changed
    
    * navbar spacer and height changed
    
    * final styling adjustments
    
    * drawer width updates based on font size
    
    * theme names no longer used
    
    * eof spacing added
    
    * widths adjusted to avoid new lines
    
    * Update hack-hate-inclusion-london/src/components/navbar/SettingsDrawer.js
    
    Co-authored-by: JessLayton <58259739+JessLayton@users.noreply.github.com>
    
    * Text changed
    
    * settings icon size increased
    
    Co-authored-by: JessLayton <58259739+JessLayton@users.noreply.github.com>

--- hack-hate-inclusion-london/src/components/navbar/Navbar.js
+++ hack-hate-inclusion-london/src/components/navbar/Navbar.js
@@ -1,15 +1,52 @@
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
+import { AppBar, Toolbar, Typography, Grid, IconButton, makeStyles, Tooltip } from '@material-ui/core';
+import SettingsIcon from '@material-ui/icons/Settings';
+
+import SettingsDrawer from './SettingsDrawer';
+
+const useStyles = makeStyles({
+  navbarSpacer: {
+    height: '100px',
+  },
+  toolBar: {
+    minHeight: '85px'
+  }
+});
 
 const Navbar = () => {
-    return (
-        <AppBar>
-            <Toolbar>
+  const classes = useStyles();
+
+  const [settingsOpen, setSettingsOpen] = React.useState(false);
+
+  const handleToggleSettings = () => {
+    setSettingsOpen(!settingsOpen);
+  };
+
+
+  return (
+    <>
+      <AppBar>
+        <Toolbar className={classes.toolBar}>
+          <Grid container direction='row' justify='space-between' alignItems='center'>
+            <Grid item>
+              <Typography variant='h3' component='h1'>
                 Hack Hate 2020
-            </Toolbar>
-        </AppBar>
-    );
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Tooltip title='Open user settings' aria-label='open user settings'>
+                <IconButton aria-label='user settings' onClick={handleToggleSettings}>
+                  <SettingsIcon color='secondary' fontSize='large'/>
+                </IconButton>
+              </Tooltip>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      <SettingsDrawer open={settingsOpen} toggleOpen={handleToggleSettings}/>
+      <div className={classes.navbarSpacer}/>
+    </>
+  );
 };
 
 export default Navbar;
