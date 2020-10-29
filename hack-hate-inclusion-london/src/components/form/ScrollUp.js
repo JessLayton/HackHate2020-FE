316a89dccb163db68d26b59a6ebdf9abfeeadd90
Jess <jessylayton@yahoo.co.uk>
2020-10-29 08:41:28 +0000

    FAB

--- hack-hate-inclusion-london/src/components/form/ScrollUp.js
+++ hack-hate-inclusion-london/src/components/form/ScrollUp.js
@@ -2,10 +2,20 @@ import React from "react";
 
 import FAB from "@material-ui/core/FAB";
 import NavigationIcon from "@material-ui/icons/Navigation";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  fab: {
+    position: "fixed",
+    bottom: "40px",
+    right: "40px",
+  },
+}));
 
 const ScrollUp = () => {
+  const classes = useStyles();
   return (
-    <FAB color="primary">
+    <FAB className={classes.fab} color="primary">
       <NavigationIcon />
     </FAB>
   );
