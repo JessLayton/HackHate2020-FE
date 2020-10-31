1b0d5f31888a31031e29df0fe57ffaf48f4d57a1
lukecottenham <36683023+lukecottenham@users.noreply.github.com>
2020-10-31 13:29:03 +0000

    snackbar added (#8)
    * snackbar added
    
    * unused classes removed
    
    * added success colour to theme
    
    * reverted orgentry changes
    
    * more reverting

--- hack-hate-inclusion-london/src/App.js
+++ hack-hate-inclusion-london/src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import Landing from './components/Landing';
 import OrgEntry from './components/OrgEntry';
+import Snackbar from './snackbar/Snackbar';
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Snackbar />
       <Switch>
         <Route exact path='/'>
           <Landing />
---
+++ hack-hate-inclusion-london/src/snackbar/Snackbar.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { observer } from 'mobx-react-lite';
+import MuiSnackbar from '@material-ui/core/Snackbar';
+import { Alert as MuiAlert, AlertTitle } from '@material-ui/lab';
+
+import SnackbarStore from './SnackbarStore';
+
+const getTitle = (severity) => {
+  switch (severity) {
+    case 'error':
+      return 'Error';
+    case 'success':
+      return 'Success';
+    default:
+      return 'Success';
+  };
+};
+
+const Alert = ({ severity, onClose, message }) => (
+  <MuiAlert
+    elevation={6}
+    variant='filled'
+    severity={severity}
+    onClose={onClose}
+  >
+    <AlertTitle>{getTitle(severity)}</AlertTitle>
+    {message}
+  </MuiAlert>
+);
+
+const Snackbar = observer(() => {
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    SnackbarStore.closeCurrentMessage();
+  };
+
+  return (
+    <MuiSnackbar open={SnackbarStore.open} autoHideDuration={10000} onClose={handleClose}>
+      <Alert
+        severity={SnackbarStore.variant}
+        message={SnackbarStore.message}
+        onClose={handleClose}
+      />
+    </MuiSnackbar>
+  );
+});
+
+export default Snackbar;
