6803dc61cf4c44d6139fe594adfbd1c0dc101d86
lukecottenham <luke.cottenham@hotmail.co.uk>
2020-11-01 08:56:32 +0000

    oops

--- hack-hate-inclusion-london/src/components/ddpo/DDPOStore.js
+++ hack-hate-inclusion-london/src/components/ddpo/DDPOStore.js
@@ -14,7 +14,7 @@ class store {
   }
 
   async loadDdpos() {
-    const resopnse = await getAllOrganisations();
+    const response = await getAllOrganisations();
     if (response && response.data && response.data.data) {
       this.ddpos = response;
     }
