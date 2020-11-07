e4863b0922899a6c30d4db1cef08697113e47ace
Jess <jessylayton@yahoo.co.uk>
2020-11-07 11:50:22 +0000

    dashLocation

--- hack-hate-inclusion-london/src/components/dash/Dash.js
+++ hack-hate-inclusion-london/src/components/dash/Dash.js
@@ -1,6 +1,8 @@
 import { Typography } from '@material-ui/core';
 import React from 'react';
 
+import { dashLocation } from '../../local/location';
+
 import { Grid, makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles({
@@ -8,7 +10,8 @@ const useStyles = makeStyles({
         marginTop: '20px',
     },
     iframe: {
-        border: 0,
+      width: '100%',
+      height: '1000px',
     }
 });
 
@@ -16,16 +19,16 @@ const Dash = () => {
     const classes = useStyles();
     return (
 
-  <Grid container className={classes.dash}>
+  <Grid container >
     <Grid container item justify='center'>
-      <Grid container item xs={11} sm={10} spacing={5}>
-        <Grid container item alignContent='center'>
+      <Grid container direction='column' item xs={11} spacing={5}>
+        <Grid item className={classes.dash}>
           <Typography variant='h4' component='h2'>
             Dashboard
           </Typography>
         </Grid>
-        <Grid container item alignContent='center'>
-          <iframe src='http://localhost:8050' width={700} height={600} title='example1' className={classes.iframe}/>
+        <Grid item>
+          <iframe src={dashLocation} title='Dashboard displaying collated data' className={classes.iframe} frameBorder={0}/>
         </Grid>
       </Grid>
     </Grid>
