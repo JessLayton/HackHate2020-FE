b03688e042c96719677e0d71f38c45858cc1ada3
Jess <jessylayton@yahoo.co.uk>
2021-05-16 22:09:08 +0100

    camelcased keys

--- hack-hate-inclusion-london/src/resources/gender.js
+++ hack-hate-inclusion-london/src/resources/gender.js
@@ -2,7 +2,7 @@ import createInitialState from '../components/form/constructForm/InitialState';
 
 const genders = [
   { label: 'Cisgender', key: 'cis' },
-  { label: 'Transgender / Nonbinary', key: 'trans_nonbinary' },
+  { label: 'Transgender / Nonbinary', key: 'transNonbinary' },
   { label: 'Gender identity not listed', key: 'unlisted' },
   { label: 'Not recorded / unknown', key: 'unknown' },
 
