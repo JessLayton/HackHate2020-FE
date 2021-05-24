7346174c8da8148a24808e1daeb96bf60907a50e
lukecottenham <luke.cottenham@hotmail.co.uk>
2021-05-24 21:59:07 +0100

    added top margin to main

--- hack-hate-inclusion-london/src/App.js
+++ hack-hate-inclusion-london/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core';
+
 import Navbar from './components/navbar/Navbar';
 import Landing from './components/landing/LandingPage';
 import Dash from './components/dash/Dash';
@@ -8,32 +10,45 @@ import ThanksPage from './components/ThanksPage';
 import OrgEntry from './components/ddpo/OrgEntry';
 import Snackbar from './snackbar/Snackbar';
 import DDPOStore from './components/ddpo/DDPOStore';
+import BottomNavigation from './components/accessible-components/BottomNavigation';
+
+const useStyles = makeStyles({
+  main: {
+    marginTop: '20px',
+  },
+});
 
 function App() {
+  const classes = useStyles();
+
   React.useEffect(() => {
     DDPOStore.loadDdpos();
   }, []);
+
   return (
     <Router>
       <Navbar />
-      <Snackbar />
-      <Switch>
-        <Route exact path='/'>
-          <Landing />
-        </Route>
-        <Route path='/dash'>
-          <Dash />
-        </Route>
-        <Route path='/form'>
-          <Form />
-        </Route>
-        <Route path='/ddpo'>
-          <OrgEntry />
-        </Route>
-        <Route path='/thankyou'>
-          <ThanksPage />
-        </Route>
-      </Switch>
+      <main className={classes.main}>
+        <Snackbar />
+        <Switch>
+          <Route exact path='/'>
+            <Landing />
+          </Route>
+          <Route path='/dash'>
+            <Dash />
+          </Route>
+          <Route path='/form'>
+            <Form />
+          </Route>
+          <Route path='/ddpo'>
+            <OrgEntry />
+          </Route>
+          <Route path='/thankyou'>
+            <ThanksPage />
+          </Route>
+        </Switch>
+      </main>
+      <BottomNavigation />
     </Router>
   );
 }
