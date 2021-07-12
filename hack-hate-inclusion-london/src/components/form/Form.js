8abdc8080c0fa21724fd78e63452b46f89b32f9a
lukecottenham <luke.cottenham@hotmail.co.uk>
2021-07-12 22:13:34 +0100

    form semantic html issues addressed

--- hack-hate-inclusion-london/src/components/form/Form.js
+++ hack-hate-inclusion-london/src/components/form/Form.js
@@ -129,10 +129,10 @@ const Form = observer(() => {
         <Brief />
         <form onSubmit={handleSubmit}>
           <ol>
-            <Grid container item direction='column' spacing={7}>
+            <li>
               <Grid container item spacing={2}>
                 <Grid item>
-                  <li>Time Period (Quarterly)</li>
+                  Time Period (Quarterly)
                 </Grid>
                 <Grid container item direction='row' spacing={2}>
                   <Grid item>
@@ -142,11 +142,15 @@ const Form = observer(() => {
                     <YearPicker value={year} onChange={setYear} required />
                   </Grid>
                 </Grid>
+                <Grid item>
+                  <Divider />
+                </Grid>
               </Grid>
-              <Divider />
+            </li>
+            <li>
               <Grid container item direction='column' spacing={2}>
                 <Grid item>
-                  <li>Name of DDPO</li>
+                  Name of DDPO
                 </Grid>
                 <Grid container item>
                   <Grid item>
@@ -156,11 +160,15 @@ const Form = observer(() => {
                     </Grid>
                   </Grid>
                 </Grid>
+                <Grid item>
+                  <Divider />
+                </Grid>
               </Grid>
-              <Divider />
+            </li>
+            <li>
               <Grid container item direction='column' spacing={2}>
                 <Grid item>
-                  <li>Boroughs Covered (Tick all that apply)</li>
+                  Boroughs Covered (Tick all that apply)
                 </Grid>
                 <Grid item>
                   <Typography>All Boroughs: </Typography>
@@ -168,175 +176,185 @@ const Form = observer(() => {
                 <Grid item>
                   <Checkboxes checkedItems={boroughs} onChange={setBoroughs} />
                 </Grid>
+                <Grid item>
+                  <Divider />
+                </Grid>
               </Grid>
-              <Divider />
+            </li>
+            <li>
               <Grid container item direction='column' spacing={2}>
                 <Grid item>
-                  <li>Details of referrals / enquiries during this quarter</li>
+                  Details of referrals / enquiries during this quarter
                 </Grid>
                 <Grid container item direction='column' spacing={2}>
                   <NumberFieldsGroup inputs={referralsAndEnquiries} value={referralsCount} onBlur={setReferralsCount} minValue={0} miniLabel={referralsAndEnquiries} />
                 </Grid>
+                <Grid item>
+                  <Divider />
+                </Grid>
               </Grid>
-              <Divider />
-              <Divider />
+            </li>
+            <li>
               <Grid container item direction='column' spacing={2}>
                 <Grid item>
-                  <li>Details of reporting during this quarter</li>
+                  Details of reporting during this quarter
                 </Grid>
                 <Grid container item direction='column' spacing={2}>
                   <NumberFieldsGroup inputs={reportingDetails} value={reportingCount} onBlur={setReportingCount} minValue={0} miniLabel={reportingDetails} />
                 </Grid>
+                <Grid item>
+                  <Divider />
+                </Grid>
               </Grid>
-              <Divider />
+            </li>
+            <li>
               <Grid container item direction='column' spacing={2}>
                 <Grid item>
-                  <li>
-                    For DDPOs who are not collecting the data outlined below,
-                    please provide a short paragraph (up to 300 words) highlighting key issues, challenges / positive outcomes.
-                  </li>
+                  For DDPOs who are not collecting the data outlined below,
+                  please provide a short paragraph (up to 300 words) highlighting key issues, challenges / positive outcomes.
                 </Grid>
                 <Grid item>
                   <BigTextBox label='Key issues/outcomes (300 words max)' value={keyIssuesPara} onBlur={setKeyIssuesPara} validate={validWordCount} />
                 </Grid>
+                <Grid item>
+                  <Divider />
+                </Grid>
               </Grid>
-              <Divider />
+            </li>
+            <li>
               <Grid container item direction='column' spacing={2}>
                 <Grid item>
-                  <li>Type of support provided - Please provide a number for each</li>
+                  Type of support provided - Please provide a number for each
                 </Grid>
                 <Grid container item direction='column' spacing={2}>
                   <NumberFieldsGroup inputs={supportTypes} value={supportCount} onBlur={setSupportCount} minValue={0} />
                 </Grid>
+                <Grid item>
+                  <Divider />
+                </Grid>
               </Grid>
-              <Divider />
+            </li>
+            <li>
               <Grid container item direction='column' spacing={2}>
                 <Grid item>
-                  <li>For cases not reported to police, please indicate main reasons why - Please provide a number for each</li>
+                  For cases not reported to police, please indicate main reasons why - Please provide a number for each
                 </Grid>
                 <Grid item>
                   <NumberFieldsGroup inputs={unreportedCases} value={unreportedCaseCount} onBlur={setUnreportedCaseCount} minValue={0} />
                 </Grid>
+                <Grid item>
+                  <Divider />
+                </Grid>
               </Grid>
-              <Divider />
+            </li>
+            <li>
               <Grid container item direction='column' spacing={2}>
                 <Grid item>
-                  <li>
-                    Intersectionality and Disability Hate Crime:
-                    Please indicate how many people you supported felt the hate crime targeted another aspect of their identity and was also a:
-                  </li>
+                  Intersectionality and Disability Hate Crime:
+                  Please indicate how many people you supported felt the hate crime targeted another aspect of their identity and was also a:
                 </Grid>
                 <Grid item>
                   <NumberFieldsGroup inputs={intersectionalCrimes} value={intersectionCrimesCount} onBlur={setIntersectionalCrimesCount} minValue={0} />
                 </Grid>
+                <Grid item>
+                  <Divider />
+                </Grid>
               </Grid>
-              <Divider />
+            </li>
+            <li>
               <Grid container item direction='column' spacing={2}>
                 <Grid item>
-                  <li>Age of victims being supported - Please provide a number</li>
+                  Age of victims being supported - Please provide a number
                 </Grid>
                 <Grid container item direction='column' spacing={2}>
                   <NumberFieldsGroup inputs={ageGroups} value={ageCount} onBlur={setAgeCount} minValue={0} />
                 </Grid>
+                <Grid item>
+                  <Divider />
+                </Grid>
               </Grid>
-              <Divider />
+            </li>
+            <li>
               <Grid container item direction='column' spacing={2}>
                 <Grid item>
-                  <li>Ethnicity of victims being supported - Please provide a number</li>
+                  Ethnicity of victims being supported - Please provide a number
                 </Grid>
                 <Grid container item direction='column' spacing={2}>
                   <NumberFieldsGroup inputs={ethnicities} value={ethnicityCount} onBlur={setEthnicityCount} minValue={0} />
                 </Grid>
-              </Grid>
-              <Divider />
-              <Grid container item direction='column' spacing={2}>
                 <Grid item>
-                  <li>What number of your clients are:  </li>
+                  <Divider />
                 </Grid>
-                <ol type='a'>
-                  <Grid container item direction='column' spacing={5}>
-                    <Grid container item direction='column' spacing={2}>
-                      <Grid item>
-                        <li> </li>
-                      </Grid>
-                      <Grid container item direction='column' spacing={2}>
-                        <NumberFieldsGroup inputs={sex} value={sexCount} onBlur={setSexCount} minValue={0} />
-                      </Grid>
-                    </Grid>
-                    <Grid container item direction='column' spacing={2}>
-                      <Grid item>
-                        <li> </li>
-                      </Grid>
-                      <Grid container item direction='column' spacing={2}>
-                        <NumberFieldsGroup inputs={orientations} value={orientationsCount} onBlur={setOrientationCount} minValue={0} />
-                      </Grid>
-                    </Grid>
-                    <Grid container item direction='column' spacing={2}>
-                      <Grid item>
-                        <li> </li>
-                      </Grid>
-                      <Grid container item direction='column' spacing={2}>
-                        <NumberFieldsGroup inputs={genders} value={genderCount} onBlur={setGenderCount} minValue={0} />
-                      </Grid>
-                    </Grid>
-                  </Grid>
-                </ol>
               </Grid>
-              <Divider />
+            </li>
+            <li>
               <Grid container item direction='column' spacing={2}>
                 <Grid item>
-                  <li>
-                    Details of where the hate crime occurred, the actions involved, and what they were regarding?
-                    (This is to highlight the multiple issues a victim can experience)
-                  </li>
+                  What number of your clients are:
                 </Grid>
-                <ol type='a'>
-                  <Grid container item direction='column' spacing={5}>
-                    <Grid container item direction='column' spacing={2}>
-                      <Grid item>
-                        <li>Where the hate crime occurred: </li>
-                      </Grid>
-                      <Grid container item direction='column' spacing={2}>
-                        <NumberFieldsGroup inputs={whereDetails} value={whereCount} onBlur={setWhereCount} minValue={0} />
-                      </Grid>
-                    </Grid>
-                    <Grid container item direction='column' spacing={2}>
-                      <Grid item>
-                        <li>Form of hate crime: </li>
-                      </Grid>
-                      <Grid container item direction='column' spacing={2}>
-                        <NumberFieldsGroup inputs={hateCrime} value={hateCrimeCount} onBlur={setHateCrimeCount} minValue={0} />
-                      </Grid>
-                    </Grid>
-                    <Grid container item direction='column' spacing={2}>
-                      <Grid item>
-                        <li>Hate crime committed by: </li>
-                      </Grid>
-                      <Grid container item direction='column' spacing={2}>
-                        <NumberFieldsGroup inputs={committedBy} value={committedByCount} onBlur={setCommittedByCount} minValue={0} />
-                      </Grid>
-                    </Grid>
-                    <Grid container item direction='column' spacing={2}>
-                      <Grid item>
-                        <li>How many were specifically regarding (relating to current/key issues): </li>
-                      </Grid>
-                      <Grid container item direction='column' spacing={2}>
-                        <NumberFieldsGroup inputs={currentIssues} value={currentIssuesCount} onBlur={setCurrentIssuesCount} minValue={0} />
-                      </Grid>
-                    </Grid>
-                  </Grid>
-                </ol>
+                <Grid item>
+                  <ol type='a'>
+                    <li>
+                      <NumberFieldsGroup inputs={sex} value={sexCount} onBlur={setSexCount} minValue={0} />
+                    </li>
+                    <li>
+                      <NumberFieldsGroup inputs={orientations} value={orientationsCount} onBlur={setOrientationCount} minValue={0} />
+                    </li>
+                    <li>
+                      <NumberFieldsGroup inputs={genders} value={genderCount} onBlur={setGenderCount} minValue={0} />
+                    </li>
+                  </ol>
+                </Grid>
+                <Grid item>
+                  <Divider />
+                </Grid>
               </Grid>
-              <Divider />
+            </li>
+            <li>
               <Grid container item direction='column' spacing={2}>
                 <Grid item>
-                  <li>What number of your clients identify the following as their impairment / condition?  </li>
+                  Details of where the hate crime occurred, the actions involved, and what they were regarding?
+                  (This is to highlight the multiple issues a victim can experience)
+                </Grid>
+                <Grid item>
+                  <ol type='a'>
+                    <li>
+                      Where the hate crime occurred:
+                      <NumberFieldsGroup inputs={whereDetails} value={whereCount} onBlur={setWhereCount} minValue={0} />
+                    </li>
+                    <li>
+                      Form of hate crime:
+                      <NumberFieldsGroup inputs={hateCrime} value={hateCrimeCount} onBlur={setHateCrimeCount} minValue={0} />
+                    </li>
+                    <li>
+                      Hate crime committed by:
+                      <NumberFieldsGroup inputs={committedBy} value={committedByCount} onBlur={setCommittedByCount} minValue={0} />
+                    </li>
+                    <li>
+                      How many were specifically regarding (relating to current/key issues):
+                      <NumberFieldsGroup inputs={currentIssues} value={currentIssuesCount} onBlur={setCurrentIssuesCount} minValue={0} />
+                    </li>
+                  </ol>
+                </Grid>
+                <Grid item>
+                  <Divider />
+                </Grid>
+              </Grid>
+            </li>
+            <li>
+              <Grid container item direction='column' spacing={2}>
+                <Grid item>
+                  What number of your clients identify the following as their impairment / condition?
                 </Grid>
                 <Grid container item direction='column' spacing={2}>
                   <NumberFieldsGroup inputs={impairments} value={impairmentCount} onBlur={setImpairmentCount} minValue={0} />
                 </Grid>
+                <Grid item>
+                  <Divider />
+                </Grid>
               </Grid>
-              <Divider />
+            </li>
+            <li>
               <Grid container item direction='column' spacing={2}>
                 <Grid item>
                   <Typography>
@@ -349,29 +367,40 @@ const Form = observer(() => {
                     Please also include what other services within your DDPO the client accessed, or any other services you signposted / referred to.
                   </Typography>
                 </Grid>
-                <Grid container item direction='column' spacing={2}>
-                  <Grid item>
-                    <li>Brief case study highlighting emotional impact of Disability Hate Crime and / or challenges / positives dealing with Police / CPS</li>
-                  </Grid>
-                  <Grid item>
-                    <BigTextBox label='Case Study (Up to 300 words)' value={emotionalImpactCS} onBlur={setEmotionalImpactCS} validate={validWordCount} />
-                  </Grid>
-                  <Grid item>
-                    <li>Brief case study highlighting achieving positive outcomes without a report to police</li>
-                  </Grid>
-                  <Grid item>
-                    <BigTextBox label='Case Study (Up to 300 words)' value={outcomesCS} onBlur={setOutcomesCS} validate={validWordCount} />
-                  </Grid>
-                </Grid>
               </Grid>
-              <Divider />
-              <Grid item>
-                <Button type='submit' variant='contained' color='primary' size='large'>
-                  Submit
-                </Button>
-              </Grid>
-            </Grid>
+              <ol type='a'>
+                <li>
+                  <Grid container item direction='column' spacing={2}>
+                    <Grid item>
+                      Brief case study highlighting emotional impact of Disability Hate Crime and / or challenges / positives dealing with Police / CPS
+                    </Grid>
+                    <Grid item>
+                      <BigTextBox label='Case Study (Up to 300 words)' value={emotionalImpactCS} onBlur={setEmotionalImpactCS} validate={validWordCount} />
+                    </Grid>
+                    <Grid item>
+                      <Divider />
+                    </Grid>
+                  </Grid>
+                </li>
+                <li>
+                  <Grid container item direction='column' spacing={2}>
+                    <Grid item>
+                      Brief case study highlighting achieving positive outcomes without a report to police
+                    </Grid>
+                    <Grid item>
+                      <BigTextBox label='Case Study (Up to 300 words)' value={outcomesCS} onBlur={setOutcomesCS} validate={validWordCount} />
+                    </Grid>
+                    <Grid item>
+                      <Divider />
+                    </Grid>
+                  </Grid>
+                </li>
+              </ol>
+            </li>
           </ol>
+          <Button type='submit' variant='contained' color='primary' size='large'>
+            Submit
+          </Button>
         </form>
       </Grid>
     </Grid>
