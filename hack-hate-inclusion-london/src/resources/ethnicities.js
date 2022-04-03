4f131a06ce1de21713034c80b0bded10f082f34e
Jess <jessylayton@yahoo.co.uk>
2022-04-03 14:21:30 +0100

    updated ethnicities

--- hack-hate-inclusion-london/src/resources/ethnicities.js
+++ hack-hate-inclusion-london/src/resources/ethnicities.js
@@ -1,27 +1,15 @@
 import createInitialState from '../components/form/constructForm/InitialState';
 
 const ethnicities = [
-  { label: 'English / Welsh / Scottish / Northern Irish / British', key: 'whiteBritish' },
-  { label: 'Irish', key: 'whiteIrish' },
-  { label: 'Irish Traveller / White Traveller Community', key: 'whiteTraveller' },
-  { label: 'White background not listed above', key: 'whiteOther' },
-  { label: 'White and Black Caribbean', key: 'mixedCaribbean' },
-  { label: 'White and Black African', key: 'mixedAfrican' },
-  { label: 'White and Asian', key: 'mixedAsian' },
-  { label: 'Mixed / Multiple ethnic backgrounds not listed above', key: 'mixedMultiple' },
-  { label: 'Indian', key: 'asianIndian' },
-  { label: 'Pakistani', key: 'asianPakistani' },
-  { label: 'Bangladeshi', key: 'asianBangladeshi' },
-  { label: 'Chinese', key: 'asianChinese' },
-  { label: 'Asian background not listed above', key: 'asianOther' },
-  { label: 'African', key: 'blackAfrican' },
-  { label: 'Caribbean', key: 'blackCaribbean' },
-  { label: 'Black / African / Caribbean background not listed above', key: 'blackOther' },
-  { label: 'Arab', key: 'arabOther' },
-  { label: 'Romani', key: 'romani' },
-  { label: 'Traveller Community not listed above', key: 'travellerOther' },
-  { label: 'Ethnic group not listed above', key: 'other' },
-  { label: 'Not recorded / unknown', key: 'unknown' },
+  { label: 'Asian', key: 'asian' },
+  { label: 'Black', key: 'black' },
+  { label: 'Latino', key: 'latino' },
+  { label: 'Multiracial - Asian and Black', key: 'asianBlack' },
+  { label: 'Multiracial - Asian and White', key: 'asianWhite' },
+  { label: 'Multiracial - Black and White', key: 'blackWhite' },
+  { label: 'Multiracial - Not listed', key: 'multiracial' },
+  { label: 'White', key: 'white' },
+  { label: 'Not Listed Elsewhere', key: 'notListed' },
 ];
 export const initialisedEthnicities = createInitialState(ethnicities);
 
